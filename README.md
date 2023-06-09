5d13d1d06737131669524b7369f62178bbb0caf4
Casey Kelso <casey@caseykelso.com>
2023-06-08 17:58:37 -0700

    ryan's example

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # test msgpack serialization of floats
+
+## How-To
+```bash
+git clone git@github.com:caseykelso/messagepack-examples.git
+cd messagepack-examples
+make build
+make run
+```
+
--- source/example.cpp
+++ source/example.cpp
@@ -1,8 +1,30 @@
 #include <string>
-//#include <msgpack.hpp>
+#include <msgpack.hpp>
+#include <iostream>
+
+void ryan_code()
+{ 
+    msgpack::sbuffer sbuf;
+    msgpack::packer<msgpack::sbuffer> pk(&sbuf);
+    float x = 10.1f;
+    float y = 20.0f;
+
+    pk.pack_map(2);
+    pk.pack(std::string("x"));
+    pk.pack(x);
+    pk.pack(std::string("y"));
+    pk.pack(y);
+
+    msgpack::object_handle oh = msgpack::unpack(sbuf.data(), sbuf.size());
+    msgpack::object obj = oh.get();
+
+    std::ostringstream msg;
+    msg << obj;
+    std::cout << msg.str() << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
-
+    ryan_code();
     return EXIT_SUCCESS;
 }
